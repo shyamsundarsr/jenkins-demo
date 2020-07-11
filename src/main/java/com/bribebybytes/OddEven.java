cea9c6ee7901d90a5da963d01d11d08a1f79f739
Rajesh K Murali <rkm.rajeshkmurali@ggmail.com>
2020-07-11 10:16:11 +0530

    Pushing the code

---
+++ src/main/java/com/bribebybytes/OddEven.java
@@ -0,0 +1,24 @@
+package com.bribebybytes;
+
+public class OddEven {
+	
+
+	public boolean isEvenNumber(int number){
+		
+		boolean result = false;
+       if(number%2 == 0){
+           result = true;
+       }
+       return result;
+    }
+	
+	public boolean isOddNumber(int number){
+		
+		boolean result = false;
+       if(number%2 != 0){
+           result = true;
+       }
+       return result;
+    }
+	
+}
---
+++ src/test/java/com/bribebybytes/OddEvenTest.java
@@ -0,0 +1,21 @@
+package com.bribebybytes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OddEvenTest {
+
+	@Test
+	public void evenTest() {
+		OddEven meo = new OddEven();
+        assertEquals("Even number", true, meo.isEvenNumber(13));
+	}
+	
+	@Test
+	public void oddTest() {
+		OddEven meo = new OddEven();
+        assertEquals("Odd number", true, meo.isOddNumber(14));
+	}
+
+}
